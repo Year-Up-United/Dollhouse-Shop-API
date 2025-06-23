fec2f37162552193e5129a4f69a435710edeaaf7
thejaylajones <janeissaj@gmail.com>
2025-06-23 16:30:41 -0400

    finished categories controller

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,23 +1,27 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.sql.SQLException;
 import java.util.List;
+
 
 // annotation for spring-boot that makes the class a controller
 @RestController
-
 // add the annotation to make this controller the endpoint for the following url
 // http://localhost:8080/categories
-@RequestMapping("/categories")
-
-
+@RequestMapping("categories")
 // add annotation to allow cross site origin requests
 @CrossOrigin
+
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -25,22 +29,31 @@ public class CategoriesController
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
+    @Autowired
     // add the appropriate annotation for a get action
-
     @GetMapping
-    public List<Category> getAll()
-    {
+    @PreAuthorize("permitAll()")
+    public List<Category> getAll() {
         // find and return all categories
-        return categoryDao.getAllCategories();
+        try {
+            return categoryDao.getAllCategories();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    // add the appropriate annotation for a get action
 
+    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        try {
+            return categoryDao.getById(id);
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "OOPS, OUR BAD!");
+        }
     }
 
     // the url to return all products in category 1 would look like this
@@ -53,25 +66,41 @@ public class CategoriesController
     }
 
     // add annotation to call this method for a POST action
+    @PostMapping()
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
-        return null;
+        try {
+            return categoryDao.create(category);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "OOPS, OUR BAD!");
+        }
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    @PostMapping({"id"})
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+        try {
+            categoryDao.update(id, category);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "OOPS, OUR BAD!");
+        }
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    @DeleteMapping("{id}")
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
         // delete the category by id
+        categoryDao.delete(id);
     }
 }
