bfa327cce8a959536d3c8d67010bcae4debc987d
thejaylajones <janeissaj@gmail.com>
2025-06-25 14:54:30 -0400

    testing method for search

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -23,11 +23,15 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color)
     {
         List<Product> products = new ArrayList<>();
+        // need string builder for searching
+        StringBuilder sql = new StringBuilder("SELECT * FROM products WHERE 1=1");
+        List<Object> parameters = new ArrayList<>();
 
-        String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (color = ? OR ? = '') ";
+        // query is wrong!!!
+       // String sql = "SELECT * FROM products " +
+//                "WHERE (category_id = ? OR ? = -1) " +
+//                "   AND (price <= ? OR ? = -1) " +
+//                "   AND (color = ? OR ? = '') ";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
